d6479a47ff09130208b70971e63207503da9e83e
jqn <jqngl@icloud.com>
2021-01-26 09:23:29 +0100

    Renamed app's entry point from flaskmongo.py to run.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
 
 # connect to database
 client = MongoClient(
-    'mongodb+srv://danielhaukaas:mongo1234509D@cluster0.zy54o.mongodb.net')
+    'mongodb+srv://{username}:{password}@cluster0.zy54o.mongodb.net'.format(username=os.getenv('DB_USERNAME'),  password=os.getenv('DB_PASSWORD')))
 
 # connect to gameDB database
 db = client.gameDB
