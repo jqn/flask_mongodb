0d30fe61cb88f2a00b6e87bdadd9669242ff1126
jqn <jqngl@icloud.com>
2021-02-03 08:41:09 +0100

    Completed base package app

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,33 @@
+import sentry_sdk
+import os
 from flask import Flask
 from pymongo import MongoClient
-import os
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 app = Flask(__name__)
+app.config.from_object('config')
+
+# Initialize sentry
+sentry_sdk.init(
+    dsn=app.config["SENTRY_DSN"],
+    integrations=[FlaskIntegration()],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # By default the SDK will try to use the SENTRY_RELEASE
+    # environment variable, or infer a git commit
+    # SHA as release, however you may want to set
+    # something more human-readable.
+    # release="myapp@1.0.0",
+)
 
 # connect to database
 client = MongoClient(
-    'mongodb+srv://{username}:{password}@cluster0.zy54o.mongodb.net'.format(username=os.getenv('DB_USERNAME'),  password=os.getenv('DB_PASSWORD')))
+    'mongodb+srv://{username}:{password}@cluster0.zy54o.mongodb.net'
+    .format(username=os.getenv('DB_USERNAME'),  password=os.getenv('DB_PASSWORD')))
 
 # connect to gameDB database
 db = client.gameDB
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from app import app
 from app import db
 import pymongo
 from flask import jsonify, request
+from sentry_sdk import capture_exception
 
 version = "1.0"
 
@@ -9,20 +10,24 @@ version = "1.0"
 @app.route('/')
 @app.route('/index')
 def index():
-    # collect teams collection
-    teams = db.teams
-    output = []
-    for team in teams.find({}, sort=[('get_players_timestamp', pymongo.ASCENDING)]).limit(10):
-        # convert record id object into string
-        # to avoid TypeError: Object of type 'ObjectId' is not JSON serializable
-        # or the need of a JSON encoder
-        team_id = str(team["_id"])
-        team["_id"] = team_id
-        output.append(team)
-        r_id, *_ = team.values()
-        print(r_id)
+    try:
+        # collect teams collection
+        teams = db.teams
+        output = []
+        for team in teams.find({}, sort=[('get_players_timestamp', pymongo.ASCENDING)]).limit(10):
+            # convert record id object into string
+            # to avoid TypeError: Object of type 'ObjectId' is not JSON serializable
+            # or the need of a JSON encoder
+            team_id = str(team["_id"])
+            team["_id"] = team_id
+            output.append(team)
+            r_id, *_ = team.values()
+            print(r_id)
 
-    return jsonify({'result': output}), 200
+        return jsonify({'result': output}), 200
+    except Exception as e:
+        # Log error to sentry
+        capture_exception(e)
 
 
 @app.route(f'/api/{version}/matches/daily/')
@@ -40,3 +45,9 @@ def matches():
         output.append(match)
 
     return jsonify({'result': output}), 200
+
+
+@app.route('/debug-sentry')
+def trigger_error():
+    # Test sentry logs
+    division_by_zero = 1 / 0
